de6e1bb2a74028875662a72f21c4a1fe5f3229c3
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-11-23 18:52:13 +0100

    created builders and getter

--- src/physics_starlinetor/example.py
+++ src/physics_starlinetor/example.py
@@ -1,5 +1,16 @@
 from vectors import vector2d
 
-speed = vector2d(1,10)
-
+#empty vector
+speed = vector2d()
 print(speed.components)
+
+#declared x and y vector
+speed = vector2d.from_x_y(x = 10, y = 1)
+print(speed.components)
+
+#declared module and angle vector
+speed = vector2d.from_module_angle(module = 10, angle = 4)
+print(speed.components)
+
+#get components
+print(speed.get_components("x", "y"))
--- src/physics_starlinetor/vectors.py
+++ src/physics_starlinetor/vectors.py
@@ -1,10 +1,44 @@
+import math
+
+from numpy import append
+
+
 class vector2d:
     
-    components : dict[str:float] = {
-        "x" : 0,
-        "y" : 0
-    }
+    #data
+    components : dict[str:float] = {}
     
-    def __init__(self, x : float, y : float):
-        self.components["x"] = x
-        self.components["y"] = y
+    #builders
+    def __init__(self):
+        """
+        Empty vector
+        """
+        self.components["x"] = 0
+        self.components["y"] = 0
+    
+    @classmethod
+    def from_x_y(cls, x : float = 0, y : float = 0):
+        """
+        Vector declared using x and y components
+        """
+        vector = cls()
+        vector.components["x"] = x
+        vector.components["y"] = y
+        return vector
+    
+    @classmethod    
+    def from_module_angle(cls, module : float = 0 , angle : float = 0):
+        """
+        Vector declared using module and angle
+        """
+        vector = cls()
+        vector.components["x"] = module * math.cos(angle)
+        vector.components["y"] = module * math.sin(angle)
+        return vector
+    
+    #getters
+    def get_components(self,*keys : str) -> list[float]:
+        components_list : list[float] = []
+        for key in keys:
+            components_list.append(self.components[key])
+        return components_list
