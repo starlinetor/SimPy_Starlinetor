26f3153e6976a9a57f40f917cb08acda65556307
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-12-27 11:50:44 +0100

    added f11 to renderer for full screen

--- src/simpy_starlinetor/rendering_starlinetor/exemple.py
+++ src/simpy_starlinetor/rendering_starlinetor/exemple.py
@@ -9,7 +9,7 @@ class gravity_sim():
         self.g = -100
         self.delta_t = 1/3600
         self.particle_1 : particle = particle(position = vector2d.from_x_y(100,800), speed = vector2d.from_x_y(100,-400), delta_t=self.delta_t)
-        self.particles = [self.particle_1]
+        self.particles = [self.particle_1, particle(vector2d(), vector2d(), 0)]
         self.pr2d = particle_renderer_2d(self.particles, vector2d(), 10)
     
     def start(self):
@@ -20,8 +20,6 @@ class gravity_sim():
         self.particle_1.update()
     
     def end(self):
-        x,y = gravity_sim_1.particle_1.position.get_components("x","y")
-        print(x,y)
         self.pr2d.update()
 
 gravity_sim_1 = gravity_sim()
@@ -30,6 +28,3 @@ fh = frame_handler(main_class = gravity_sim_1, target_fps = 60, executions_pf = 
 
 while True:
     fh.basic()
-
-while True:
-    pr2d.update()
--- src/simpy_starlinetor/rendering_starlinetor/particle_renderer.py
+++ src/simpy_starlinetor/rendering_starlinetor/particle_renderer.py
@@ -4,16 +4,22 @@ from simpy_starlinetor.rendering_starlinetor.rendering_utils import *
 from simpy_starlinetor.sim_objects_starlinetor.particles_starlinetor.particle import *
 
 class particle_renderer_2d:
+    
+    def full_screen(self, eventorigin):
+        self.full_screen_var = not self.full_screen_var
+        self.root.attributes("-fullscreen",self.full_screen_var)
+    
     def __init__(self, particles : list[particle], resolution : vector2d, particle_radius : float):
         """
         Functionality:
-        TODO reset to 0/0 and 100% zoom
+        R to reset to 0/0 and 100% zoom
         TODO Zoom : starting zoom is 100%
             scroll wheel 
         TODO Move around : ability to move around, this needs some type of chamera position
             left click drag
         TODO Adaptive resolution : this means that the rendering will be separated from the resolution
         TODO Render particles : draws circle where particles should be
+        F11 -> full screen
         """
         #rendering variables
         self.particles : list[particle] = particles
@@ -22,19 +28,24 @@ class particle_renderer_2d:
         self.zoom : int = 100
         #this rappresents the radius of particles, is based on the simulation size and not rendering size
         self.particle_radius : float = particle_radius
+        #full screen
+        self.full_screen_var = True
         
         #initialize tk
         self.root = tk.Tk()
-        self.canvas = tk.Canvas(self.root, bg="white",height=1920, width=1080)
+        self.canvas = tk.Canvas(self.root, bg="white",height=1080, width=1920)
         self.canvas.pack()
-        self.root.attributes("-fullscreen", True)
+        
+        #mapping of all keys
+        #f11 fullscreen
+        self.root.bind("<F11>", self.full_screen)
         
         #initialize particles
         #stores the id of each particle
         self.tk_particles : list[int] = []
 
-        for element in self.particles:
-            self.tk_particles.append(tk_create_oval(self.canvas, element.get_position(), self.particle_radius))
+        for _ in self.particles:
+            self.tk_particles.append(tk_create_oval(self.canvas, vector2d(), self.particle_radius))
     
     def update(self):
         """"
